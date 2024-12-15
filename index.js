ab10847d3977774ccda2f60a54f05cce49ea89a8
juliapaesv <juliapaesv@gmail.com>
2024-12-15 13:07:28 -0300

    Commit 9 - Criando Arquivos

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const { readFileSync } = require('fs');
-const Repositorio = require('./repositorio');
-const ServicoCalculoFatura = require('./calculoFatura');
-const { formatarMoeda } = require('./utils');
-
-function gerarFaturaStr(fatura, calc) {
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
-    for (let apre of fatura.apresentacoes) {
-        faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
-    }
-    faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
-    faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} \n`;
-    return faturaStr;
-}
+var Repositorio = require("./repositorio.js");
+var ServicoCalculoFatura = require("./servico.js");
+var gerarFaturaStr = require("./apresentacao.js");
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
-
 const calc = new ServicoCalculoFatura(new Repositorio());
-
 const faturaStr = gerarFaturaStr(faturas, calc);
 console.log(faturaStr);
